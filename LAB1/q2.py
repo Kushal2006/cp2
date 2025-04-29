964d9b8ca8038b14d0f77d7b30d9495a9119eb05
Kushal2006 <97942646+Kushal2006@users.noreply.github.com>
2025-04-29 23:12:04 +0530

    Update q2.py

--- LAB1/q2.py
+++ LAB1/q2.py
@@ -1,3 +1,6 @@
+print("Name: Kushal Dubey")
+print("Roll No: 24BEE110")
+
 a = int(input("Enter the marks of first subject"))
 b = int(input("Enter the marks of second subject"))
 c = int(input("Enter the marks of third subject"))
